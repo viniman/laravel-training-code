5ad9b9f81af78a65eaf35c08789f456233f71f24
viniman <vinicius.carlos@live.com>
2020-09-16 15:28:07 -0300

    Update project

--- database/migrations/2020_09_16_102653_create_courses_table.php
+++ database/migrations/2020_09_16_102653_create_courses_table.php
@@ -15,6 +15,12 @@ class CreateCoursesTable extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('slug');
+            $table->string('image');
+            $table->string('video');
+            $table->string('category');
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Route::middleware('auth')->group(function(){
     })->name('dashboard');
     Route::get('/home', 'HomeController@index')->name('home');
     Route::resource('/users','UserController');
+    Route::resource('/categories', 'CategoryController');
+    Route::resource('/courses', 'CategoryController');
 });
